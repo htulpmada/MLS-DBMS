d03336c5b2fa432eb43ece7505ffa8e279fcd6ba
Adam Pluth <Apluth@crimson.ua.edu>
2017-04-29 16:41:59 -0500

    read in files and kind of error check

---
+++ QPluth/src/qpluth/QDataReader.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qpluth;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gomez
+ */
+public class QDataReader {
+    
+    int lineNum = 0;
+    int charNum = 0;
+    
+    public QDataReader(){}
+
+    public ArrayList<ArrayList<String>> read(String a) {
+        lineNum = 0;
+        charNum = 0;
+        ArrayList<ArrayList<String>> data = new ArrayList<>();
+        ArrayList<String> dataLine = new ArrayList<>();
+         try {
+            for (String line : Files.readAllLines(Paths.get(a))) {
+                lineNum++;
+                for (String part : line.split("\\s+")) {
+                    charNum++;
+                    dataLine.add(part);
+                }
+                data.add(dataLine);
+                dataLine = new ArrayList<>();
+            }
+//            printData(data);
+            return data;
+        } catch (IOException ex) {
+            System.out.print("Error Finding File: " + a + "\n");
+            System.exit(1);
+        } catch (NullPointerException n) {
+            System.out.print("Error Finding File: " + a + "\n");
+            System.exit(1);
+        }
+        return data;
+    }
+    
+    public void printData(ArrayList<ArrayList<String>> arr){
+        for(ArrayList<String> ar : arr){
+            for(String a : ar){
+                System.out.print( a +"\t");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
--- QPluth/src/qpluth/QPluth.java
+++ QPluth/src/qpluth/QPluth.java
@@ -5,19 +5,67 @@
  */
 package qpluth;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  *
  * @author APluth
  */
 public class QPluth {
 
+    static String[] data = {"T1.txt","T2.txt","T3.txt"};
+    File t1,t2,t3;
+    Qlexeme query = null;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ArrayList<ArrayList<ArrayList<String>>> t; // 3D array for tables of data
+        // read in files 
+        // from command line? or default files
+        if(checkInput(args)){
+           t = readFile(args);
+        }
+        else{
+           t = readFile(data);
+        }
+        // promt for security level
         
-
+        //process queries until exit
 
     }
+    
+    public static ArrayList<ArrayList<ArrayList<String>>> readFile(String[] args){
+        ArrayList<ArrayList<ArrayList<String>>> tables = new ArrayList<>();
+        QDataReader dReader = new QDataReader();
+            
+        for(String a : args){
+//            System.out.print("NEW Table: \n");
+            tables.add(dReader.read(a));
+        }
+        
+        return tables;
+    }
+    
+    public static boolean checkInput(String[] args){
+        final boolean status = false;
+        if(args.length<3){
+            System.out.print("We Need More Than Two Arguments, Proceeding with Defualt Data\n");
+            for(String a:args){System.out.print(a + "\n");}
+            return status;
+        }
+        else if(args[0].endsWith(".txt")&&args[1].endsWith(".txt")&&args[2].endsWith(".txt")){
+            // all three have to be .txt
+            System.out.print("Proceeding with Data Files:\n");
+            for(String a:args){System.out.print("\t"+a+"\n");}
+            return !status;
+        }
+        else{
+            System.out.print("Sorry We need Three (or more?) .txt Files\n~~~~~~~~`Maybe More in the Future~~~~\n");
+            return status;
+        }
+    }
+
     
 }
